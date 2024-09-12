5df62fc1dbb291eeeca477b842d4740956355757
user name <at-eidfstaff@eidf114-aturner.vms.os.eidf.epcc.ed.ac.uk>
2024-09-12 09:50:55 +0000

    Cirrus CPU rigid output

---
+++ run-simulation.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+model=$1
+
+# Cleanup
+rm -f $PWD/$model/data/output/*.csv
+
+docker run -v $PWD/$model/data:/data -v $PWD/algorithm:/algorithm --name $model --detach elastisim /data/input/configuration.json --log=root.thresh:warning 
+sleep 20
+docker container ls
+echo "[$(date)] Starting running $model"
+
+docker exec $model python3 /algorithm/algorithm.py
+
+echo "[$(date)] Finished running $model"
+
+docker logs $model
+
+
