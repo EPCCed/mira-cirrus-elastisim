fd178e35dfa79d965978633b7342c4731be557ae
Andy Turner <a.turner@epcc.ed.ac.uk>
2024-10-29 13:55:20 +0000

    Full list of jobs

--- utils/sort_json_submit.py
+++ utils/sort_json_submit.py
@@ -5,20 +5,16 @@ infile = sys.argv[1]
 
 f = open(infile)
 jobs_d = json.load(f)
+print(len(jobs_d['jobs']))
 
-index_jobs_d = {}
-for job in jobs_d['jobs']:
-    st = job['submit_time']
-    index_jobs_d[st] = job
+jobs_a = jobs_d['jobs']
 
-index_jobs_d = dict(sorted(index_jobs_d.items()))
+jobs_sorted_a = sorted(jobs_a, key=lambda d: d['submit_time'])
+
+print(len(jobs_sorted_a))
 
 jobs_d = {}
-jobs_a = []
-for i, job in index_jobs_d.items():
-    jobs_a.append(job)
-
-jobs_d['jobs'] = jobs_a
+jobs_d['jobs'] = jobs_sorted_a
 
 with open(sys.argv[2], 'w') as fp:
     json.dump(jobs_d, fp, indent=4)
